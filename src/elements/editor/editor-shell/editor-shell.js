36d82d153b31191115f0f60315c0c80baa5ee247
Pim de Wit <dewit@dewit-macbookpro2.roam.corp.google.com>
2016-09-27 15:20:12 +0100

    stuff

--- src/elements/editor/editor-shell/editor-shell.js
+++ src/elements/editor/editor-shell/editor-shell.js
@@ -5,12 +5,44 @@ Polymer({
     loading: {
       type: String,
       value: true,
-      reflectToAttribute: true
+      reflectToAttribute: true,
+      observer: '_loadingObserver'
     },
 
     selectedMaterial: {
       type: Object,
       notify: true
+    },
+
+    material: {
+      type: Object,
+      observer: '_materialObserver'
     }
+  },
+
+  scenePath: '/src/elements/editor/editor-scene/editor-scene.html',
+
+  _loadingObserver: function() {
+    if (!this.loading) {
+      this.importHref(this.scenePath, function() {
+        this.loading = false;
+        console.log('scene init');
+      },
+      function() {
+        console.log('error');
+      });
+    }
+  },
+
+  _materialObserver: function() {
+    if (this.material) {
+      console.log(this.material);
+    }
+  },
+
+  _onMaterialListLoaded: function(response) {
+    this.materialList = response.detail.response;
+    console.log(this.materialList);
   }
+
 });
